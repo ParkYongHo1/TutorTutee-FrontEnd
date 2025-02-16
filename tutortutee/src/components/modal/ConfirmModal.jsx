5021dab1601e538f39335c1529add07587e587a3
ParkYongHo1 <qkaxhf8823@naver.com>
2025-02-16 23:15:58 +0900

    Design | 팔로워 리스트 UI 와나성

--- tutortutee/src/components/modal/ConfirmModal.jsx
+++ tutortutee/src/components/modal/ConfirmModal.jsx
@@ -5,40 +5,42 @@ const ConfirmModal = ({ isOpen, onConfirm, onCancel, message }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="w-[500px] font-bold bg-white rounded-[5px] text-black border z-30 absolute top-[50px] right-[600px]">
-      <div className="text-lg  w-full p-6">
-        <p className="text-center font-bold text-2xl w-full">회원탈퇴</p>
-        <div className="flex items-center justify-start">
-          <p className="my-[20px]">{message}</p>
-        </div>
-        <div className="flex h-[50px] bg-red--100 mb-2">
-          <p className="w-[10px] bg-red--300"></p>
-          <div className="flex items-center px-4">
-            <LazyLoadImage
-              src={`${process.env.PUBLIC_URL}/image/default/warn.svg`}
-              alt="실패 아이콘"
-              className="m-auto"
-              width={30}
-              height={30}
-            />
-            <p className="text-xs ml-2">
-              탈퇴하면 기존 데이터를 복구할수 없게 됩니다.
-            </p>
+    <div className="fixed inset-0 flex items-start justify-center z-30">
+      <div className="w-[500px] font-bold bg-white rounded-[5px] text-black border mt-[50px]">
+        <div className="text-lg w-full p-6">
+          <p className="text-center font-bold text-2xl w-full">회원탈퇴</p>
+          <div className="flex items-center justify-start">
+            <p className="my-[20px]">{message}</p>
           </div>
-        </div>
-        <div className="flex justify-end w-full text-end">
-          <button
-            onClick={onConfirm}
-            className="mr-[20px] bg-black text-white p-2 w-[100px] text-sm"
-          >
-            확인
-          </button>
-          <button
-            onClick={onCancel}
-            className="border text-black p-2 w-[100px] text-sm"
-          >
-            취소
-          </button>
+          <div className="flex h-[50px] bg-red--100 mb-2">
+            <p className="w-[10px] bg-red--300"></p>
+            <div className="flex items-center px-4">
+              <LazyLoadImage
+                src={`${process.env.PUBLIC_URL}/image/default/warn.svg`}
+                alt="실패 아이콘"
+                className="m-auto"
+                width={30}
+                height={30}
+              />
+              <p className="text-xs ml-2">
+                탈퇴하면 기존 데이터를 복구할수 없게 됩니다.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-end w-full text-end">
+            <button
+              onClick={onConfirm}
+              className="mr-[20px] bg-black text-white p-2 w-[100px] text-sm"
+            >
+              확인
+            </button>
+            <button
+              onClick={onCancel}
+              className="border text-black p-2 w-[100px] text-sm"
+            >
+              취소
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- tutortutee/src/components/profile/FollowerList.jsx
+++ tutortutee/src/components/profile/FollowerList.jsx
@@ -1,8 +1,65 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 const FollowerList = () => {
   return (
     <>
-      <div>팔로워리스트</div>
+      <p className="text-sm font-bold mb-[12px]">검색</p>
+      <div className="flex mb-[12px] items-center ">
+        <div className="relative w-full">
+          <input
+            type="text"
+            placeholder="닉네임을 입력해주세요."
+            className="w-full px-10 h-[30px] "
+          />
+          <LazyLoadImage
+            src={`${process.env.PUBLIC_URL}/image/profile/search.svg`}
+            alt="검색"
+            className="max-w-full cursor-pointer absolute top-0 left-[0px]"
+            width={30}
+            height={30}
+          />
+        </div>
+      </div>
+      <hr />
+
+      <input type="text w-[500px] h-[50px] border" />
+      <div className="text-sm font-bold">팔로워</div>
+      <hr className="my-[12px]" />
+      <div className="flex flex-col items-center overflow-y-auto max-h-[600px] scrollable">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div key={index} className="flex mt-[12px] items-center w-full">
+            <div className="w-[100px] h-[100px]">
+              <LazyLoadImage
+                src={`${process.env.PUBLIC_URL}/image/default/profile.svg`}
+                alt="프로필"
+                className="w-full h-full object-cover fill border rounded-full mb-[24px]"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="w-[400px] mx-[24px]">
+              <p className="text-lg font-bold">닉네임</p>
+              <p className="text-sm mt-[8px]">안녕하세요</p>
+            </div>
+            <div>
+              <button className="bg-blue-500 text-white font-bold w-[120px] h-[40px] rounded-[5px]">
+                맞팔로우
+              </button>
+            </div>
+            <div className="w-[20px] h-[20px] mx-[20px] cursor-pointer">
+              <LazyLoadImage
+                src={`${process.env.PUBLIC_URL}/image/default/cancel.svg`}
+                alt="프로필"
+                className=""
+                width={20}
+                height={20}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
+
 export default FollowerList;
