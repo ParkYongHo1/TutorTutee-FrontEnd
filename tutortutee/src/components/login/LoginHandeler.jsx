36dc9e52d3ac722e0c049940eb79ab3fdbbed91a
ParkYongHo1 <qkaxhf8823@naver.com>
2025-02-18 09:21:17 +0900

    Feat | 카카오 로그인 API 바인딩

--- tutortutee/src/components/login/LoginHandeler.jsx
+++ tutortutee/src/components/login/LoginHandeler.jsx
@@ -1,34 +1,39 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { login } from "../../slices/memberSlice";
+import { useDispatch } from "react-redux";
 
 const LoginHandeler = (props) => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
-
-  //인가코드 백으로 보내는 코드
+  const dispatch = useDispatch();
   useEffect(() => {
     const kakaoLogin = async () => {
-      await axios.get(
-        `${process.env.REACT_APP_BASE_URL}/oauth/kakao?code=${code}`
-      );
-      await axios({
-        method: "GET",
-        url: `${process.env.REACT_APP_REDIRECT_URL}/?code=${code}`,
-        headers: {
-          "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
-          "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
-        },
-      }).then((res) => {
-        console.log(res);
-        //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
-        localStorage.setItem("name", res.data.account.kakaoName);
-        //로그인이 성공하면 이동할 페이지
-        navigate("/owner-question");
-      });
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}/oauth/kakao?code=${code}`
+        );
+        const memberInfo = {
+          memberNum: response.data.memberNum,
+          nickname: response.data.nickname,
+          profileImg: response.data.profileImg,
+          introduction: response.data.introduction,
+          hasMore: response.data.hasMore,
+          loginType: response.data.loginType,
+        };
+        dispatch(login({ member: memberInfo, access: response.data.access }));
+        navigate("/");
+      } catch (error) {
+        if (error) {
+          {
+            alert("오류가 발생했습니다. 잠시후 다시 시도해주세요.");
+          }
+        }
+      }
     };
     kakaoLogin();
-  }, [props.history]);
+  }, [props.history, code, dispatch, navigate]);
   return (
     <>
       <div></div>
